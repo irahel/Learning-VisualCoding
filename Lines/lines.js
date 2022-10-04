561afa4cfa0ad399d5b6e9373c7ab24a511aed55
Isaac Rahel <isaac_rahel@live.com>
2022-10-04 19:07:45 -0300

    feat: coding lines

---
+++ Lines/lines.js
@@ -0,0 +1,106 @@
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const Color = require('canvas-sketch-util/color');
+
+const settings = {
+  dimensions: [ 1080, 1080 ],
+  animate: true
+};
+
+const sketch = ({ context, width, height }) => {
+  const agents = [];
+  for (let i = 0; i < 40; i++){
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agents.push(new Agent(x, y));
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < agents.length; i++){
+      const agent = agents[i];
+
+      for (let j = 0; j < agents.length; j++){
+
+        const other = agents[j];
+        if( random.range(0,1) <= 0.5) context.strokeStyle = agent.color;
+        else context.strokeStyle = other.color;
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+        console.log("something");
+      }
+    }
+
+    agents.forEach(agent => {
+      agent.update();
+      agent.draw(context);
+      agent.bounce(width, height);
+    });
+
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Vector{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y){
+    this.pos = new Vector(x, y);
+    this.speed = new Vector(random.range(-1,1), random.range(-1,1));
+    this.radius = random.range(4, 12)*2;
+    this.color = `rgba(
+      ${random.range(0, 255)},
+      ${random.range(0, 255)},
+      ${random.range(0, 255)}, 1)`;
+  }
+
+  recolor(){
+    this.color = `rgba(
+      ${random.range(0, 255)},
+      ${random.range(0, 255)},
+      ${random.range(0, 255)}, 1)`;
+  }
+
+  bounce(width, height){
+    var bounced = false;
+    if (this.pos.x <= 0 || this.pos.x >= width){
+      bounced = true;
+      this.speed.x *= -1;
+    }
+    if (this.pos.y <= 0 || this.pos.y >= height){
+      bounced = true;
+      this.speed.y *= -1;
+    }
+    if (bounced) this.recolor();
+  }
+
+  update(){
+    this.pos.x += this.speed.x;
+    this.pos.y += this.speed.y;
+  }
+
+  draw(context){
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.fillStyle = this.color;
+    context.lineWidth = 4;
+
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, Math.PI *2);
+    context.fill();
+
+    context.restore();
+  }
+}
